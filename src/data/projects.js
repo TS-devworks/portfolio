88561cce17757cfa5396afe5fbdd3494aec6fd0c
T.Smith <tjswfhome@gmail.com>
2026-05-05 10:27:21 -0400

    Background Update

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,11 +3,11 @@ export const projects = [
   {
     title: "ConfigMap",
     description: "Visualizes Zendesk trigger relationships",
-    media: "/videos/ConfigMap-v1.4.0.mp4",
+    media: "videos/ConfigMap-v1.4.0.mp4",
   },
   {
     title: "Conversation Analysis",
     description: "Analyzes conversation patterns",
-    media: "/videos/Ticket-Conversation-Analysis-v1.1.mp4",
+    media: "videos/Ticket-Conversation-Analysis-v1.1.mp4",
   },
 ];
